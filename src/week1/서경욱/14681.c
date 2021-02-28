763248f4bd77a131d0532f7f3376b7f33876f687
carmaking <79752918+carmaking@users.noreply.github.com>
2021-02-28 17:58:37 +0900

    Create 14681.c

---
+++ src/week1/서경욱/14681.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int x,y;
+    scanf("%d\n%d",&x,&y);
+    
+    if(x>0 & y>0)
+        printf("1");
+    else if(x>0 & y<0)
+        printf("4");
+    else if(x<0 & y>0)
+        printf("2");
+    else
+        printf("3");
+    
+    return 0;
+}
