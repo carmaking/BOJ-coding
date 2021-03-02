90314cbaae122c1a5ba49b114b32203959e4f6fd
carmaking <79752918+carmaking@users.noreply.github.com>
2021-03-02 13:57:56 +0900

    Create 10952.c

---
+++ src/week1/서경욱/10952.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int A=1,B=1;
+    
+    while(A){
+      scanf("%d %d",&A,&B);
+      if(A==0){
+        break;
+      }
+      printf("%d\n",A+B);
+    }
+    
+    return 0;
+}
