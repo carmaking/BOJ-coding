c8e76eed1ec53d7e8fbd71bb6931b8d5dc4c1280
carmaking <79752918+carmaking@users.noreply.github.com>
2021-03-04 13:57:27 +0900

    Create 1065.c

---
+++ src/week1/서경욱/1065.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void hansu(int n){
+    int hun,ten,one,count=0;
+    for(int i=1;i<=n;i++){
+        hun=i/100, ten=(i%100)/10, one=i%10;
+        
+        
+    }
+}
+
+int main(){
+    int N;
+    scanf("%d",&N);
+    hansu(N);
+    return 0;
+}
