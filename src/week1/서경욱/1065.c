e45eec426cbc73bee8e57bb8e6e5541ec65c1395
carmaking <79752918+carmaking@users.noreply.github.com>
2021-03-04 16:47:50 +0900

    Update 1065.c

--- src/week1/서경욱/1065.c
+++ src/week1/서경욱/1065.c
@@ -2,10 +2,15 @@
 void hansu(int n){
     int hun,ten,one,count=0;
     for(int i=1;i<=n;i++){
-        hun=i/100, ten=(i%100)/10, one=i%10;
-        
-        
+        if(i<100)
+            count++;
+        else{
+            hun=i/100, ten=(i%100)/10, one=i%10;
+            if((hun-ten)==(ten-one))
+                count++;
+        }
     }
+    printf("%d",count);
 }
 
 int main(){
